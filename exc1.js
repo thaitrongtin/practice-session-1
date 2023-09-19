8132db658938da79f4b1c8b7528a6b240e3a6476
TrongTin <trongtin812@gmail.com>
2023-09-19 09:09:54 +0700

    finalize exc1

--- exc1.js
+++ exc1.js
@@ -1,1 +1,10 @@
-sfhsjkfshjksf;
+var height;
+var weight;
+var bmi = function (height, weight) {
+  var kl = weight / (height * 2);
+  console.log(kl);
+};
+var mark = bmi(80, 168);
+var bi = bmi(95, 196);
+const markHigherBMI = mark > bi;
+console.log(markHigherBMI);
